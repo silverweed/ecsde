d2907eb1c9889ed1220cbc525ffe5ea7650ccfa0
silverweed <silverweed1991@gmail.com>
2019-11-24 15:33:41 +0100

    spawn less demo entities

--- ecs_game/src/gameplay_system.rs
+++ ecs_game/src/gameplay_system.rs
@@ -247,7 +247,7 @@ impl Gameplay_System {
 
         let mut prev_entity: Option<Entity> = None;
         let n_frames = 4;
-        for i in 0..20000 {
+        for i in 0..1000 {
             let entity = em.new_entity();
             let (sw, sh) = {
                 let mut rend = em.add_component::<C_Renderable>(entity);
