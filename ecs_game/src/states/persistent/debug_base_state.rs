2b7a5a12e16a55c7aa9cb57ca0a494e0d4decd94
silverweed <silverweed1991@gmail.com>
2019-12-21 14:34:08 +0100

    replace if-else chain with a match in debug_base_state

--- ecs_game/src/states/persistent/debug_base_state.rs
+++ ecs_game/src/states/persistent/debug_base_state.rs
@@ -45,58 +45,66 @@ impl Persistent_Game_State for Debug_Base_State {
     ) -> bool {
         let debug_ui = &mut engine_state.debug_systems.debug_ui_system;
 
-        // @Refactor: change the horrible if-else chain with a match.
-        // This requires implementing a compile-time sid function (consider syntax extension).
+        // @Speed: eventually we want to replace all the *name == sid with a const sid function, to allow doing
+        // (sid!("game_speed_up"), Action_Kind::Pressed) => { ... }
         for action in actions.iter() {
-            if (action.0 == self.sid_game_speed_up || action.0 == self.sid_game_speed_down)
-                && action.1 == Action_Kind::Pressed
-            {
-                let ts = engine_state.time.time_scale
-                    + CHANGE_SPEED_DELTA
-                        * if action.0 == self.sid_game_speed_up {
-                            1.0
-                        } else {
-                            -1.0
-                        };
-                if ts > 0.0 {
-                    engine_state.time.time_scale = ts;
+            match action {
+                (name, Action_Kind::Pressed)
+                    if *name == self.sid_game_speed_up || *name == self.sid_game_speed_down =>
+                {
+                    let ts = engine_state.time.time_scale
+                        + CHANGE_SPEED_DELTA
+                            * if action.0 == self.sid_game_speed_up {
+                                1.0
+                            } else {
+                                -1.0
+                            };
+                    if ts > 0.0 {
+                        engine_state.time.time_scale = ts;
+                    }
+                    let msg_overlay = debug_ui.get_fadeout_overlay(String_Id::from("msg"));
+                    msg_overlay
+                        .add_line(&format!("Time scale: {:.2}", engine_state.time.time_scale));
                 }
-                let msg_overlay = debug_ui.get_fadeout_overlay(String_Id::from("msg"));
-                msg_overlay.add_line(&format!("Time scale: {:.2}", engine_state.time.time_scale));
-            } else if action.0 == self.sid_pause_toggle && action.1 == Action_Kind::Pressed {
-                let msg_overlay = debug_ui.get_fadeout_overlay(String_Id::from("msg"));
-                engine_state.time.pause_toggle();
-                msg_overlay.add_line(if engine_state.time.paused {
-                    "Paused"
-                } else {
-                    "Resumed"
-                });
-            } else if action.0 == self.sid_step_sim && action.1 == Action_Kind::Pressed {
-                // FIXME: game does not resume after calling this
-                let msg_overlay = debug_ui.get_fadeout_overlay(String_Id::from("msg"));
-                let target_fps = self.fps.read(&engine_state.config);
-                let step_delta =
-                    Duration::from_nanos(u64::try_from(1_000_000_000 / target_fps).unwrap());
-                msg_overlay.add_line(&format!(
-                    "Stepping of: {:.2} ms",
-                    time::to_secs_frac(&step_delta) * 1000.0
-                ));
-                engine_state.time.paused = true;
-                engine_state.time.step(&step_delta);
-                gs.step(
-                    &step_delta,
-                    &engine_state.config,
-                    clone_tracer!(engine_state.tracer),
-                );
-            } else if action.0 == self.sid_print_em_debug_info && action.1 == Action_Kind::Pressed {
-                gs.print_debug_info();
-            } else if action.0 == self.sid_quit && action.1 == Action_Kind::Pressed {
-                return true;
-            } else if action.0 == self.sid_toggle_trace_overlay && action.1 == Action_Kind::Pressed
-            {
-                let show_trace = &mut engine_state.debug_systems.show_trace_overlay;
-                *show_trace = !*show_trace;
-                debug_ui.set_overlay_enabled(String_Id::from("trace"), *show_trace);
+                (name, Action_Kind::Pressed) if *name == self.sid_pause_toggle => {
+                    let msg_overlay = debug_ui.get_fadeout_overlay(String_Id::from("msg"));
+                    engine_state.time.pause_toggle();
+                    msg_overlay.add_line(if engine_state.time.paused {
+                        "Paused"
+                    } else {
+                        "Resumed"
+                    });
+                }
+                (name, Action_Kind::Pressed) if *name == self.sid_step_sim => {
+                    // FIXME: game does not resume after calling this
+                    let msg_overlay = debug_ui.get_fadeout_overlay(String_Id::from("msg"));
+                    let target_fps = self.fps.read(&engine_state.config);
+                    let step_delta =
+                        Duration::from_nanos(u64::try_from(1_000_000_000 / target_fps).unwrap());
+                    msg_overlay.add_line(&format!(
+                        "Stepping of: {:.2} ms",
+                        time::to_secs_frac(&step_delta) * 1000.0
+                    ));
+                    engine_state.time.paused = true;
+                    engine_state.time.step(&step_delta);
+                    gs.step(
+                        &step_delta,
+                        &engine_state.config,
+                        clone_tracer!(engine_state.tracer),
+                    );
+                }
+                (name, Action_Kind::Pressed) if *name == self.sid_print_em_debug_info => {
+                    gs.print_debug_info();
+                }
+                (name, Action_Kind::Pressed) if *name == self.sid_quit => {
+                    return true;
+                }
+                (name, Action_Kind::Pressed) if *name == self.sid_toggle_trace_overlay => {
+                    let show_trace = &mut engine_state.debug_systems.show_trace_overlay;
+                    *show_trace = !*show_trace;
+                    debug_ui.set_overlay_enabled(String_Id::from("trace"), *show_trace);
+                }
+                _ => {}
             }
         }
         false
