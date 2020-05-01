2f6138c05ace50a9ce27d28a53fd0835843e3d7b
silverweed <silverweed1991@gmail.com>
2020-05-01 11:33:12 +0200

    Revert "add stop_file_watch to runner (TODO: update notify to 5.0!)"
    This reverts commit 9cc75e68e5a4e3db270f003007b8567358ff6b26.

--- ecs_runner/src/hotload/file_watcher.rs
+++ ecs_runner/src/hotload/file_watcher.rs
@@ -1,6 +1,6 @@
-use notify::{watcher, DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use std::path::PathBuf;
-use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
+use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
 
@@ -13,59 +13,34 @@ pub struct File_Watch_Config {
     pub interval: Duration,
 }
 
-pub struct File_Watch_Thread {
-    pub handle: thread::JoinHandle<()>,
-    pub watcher: RecommendedWatcher,
-    pub stop_signal: Sender<()>,
-    path: PathBuf,
-}
-
-pub fn stop_file_watch(mut fw_thread: File_Watch_Thread) {
-    fw_thread.watcher.unwatch(&fw_thread.path).unwrap();
-    fw_thread.stop_signal.send(()).unwrap();
-    fw_thread.handle.join().unwrap();
-}
-
 pub fn start_file_watch(
     path: PathBuf,
     config: File_Watch_Config,
     event_handlers: Vec<Box<dyn File_Watcher_Event_Handler>>,
-) -> std::io::Result<File_Watch_Thread> {
-    let (wtx, wrx) = channel();
-    let (stop_tx, stop_rx) = channel();
-    let mut watcher = watcher(wtx, config.interval).unwrap();
-    watcher
-        .watch(path.to_str().unwrap(), config.recursive_mode)
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("{}", e)))?;
-    eprintln!("Started watching {:?}", path);
-
-    let handle = thread::Builder::new()
+) -> std::io::Result<thread::JoinHandle<()>> {
+    thread::Builder::new()
         .name(format!(
             "runner_fwch_{:?}",
             path.as_path().file_name().unwrap()
         ))
         .spawn(move || {
-            file_watch_listen(wrx, event_handlers, stop_rx).unwrap();
-        })?;
-
-    Ok(File_Watch_Thread {
-        path,
-        handle,
-        watcher,
-        stop_signal: stop_tx,
-    })
+            file_watch_listen(path, config, event_handlers).unwrap();
+        })
 }
 
 fn file_watch_listen(
-    watcher_rx: Receiver<DebouncedEvent>,
+    path: PathBuf,
+    config: File_Watch_Config,
     mut event_handlers: Vec<Box<dyn File_Watcher_Event_Handler>>,
-    stop_signal: Receiver<()>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    while stop_signal.try_recv() == Err(TryRecvError::Empty) {
-        notify_handlers(&mut event_handlers, watcher_rx.recv()?);
-    }
+    let (tx, rx) = channel();
+    let mut watcher = watcher(tx, config.interval).unwrap();
+    watcher.watch(path.to_str().unwrap(), config.recursive_mode)?;
+    eprintln!("Started watching {:?}", path);
 
-    Ok(())
+    loop {
+        notify_handlers(&mut event_handlers, rx.recv()?);
+    }
 }
 
 fn notify_handlers(
--- ecs_runner/src/main.rs
+++ ecs_runner/src/main.rs
@@ -4,6 +4,9 @@
 
 extern crate libloading as ll;
 
+#[cfg(debug_assertions)]
+extern crate notify;
+
 mod game_api;
 mod hotload;
 
@@ -40,14 +43,14 @@ macro_rules! get_argc_argv {
 #[cfg(debug_assertions)]
 fn main() -> std::io::Result<()> {
     use std::path::PathBuf;
-    use std::sync::mpsc::{channel, sync_channel};
+    use std::sync::mpsc::sync_channel;
 
     let game_dll_abs_path = format!("{}/{}", GAME_DLL_FOLDER, GAME_DLL_FILE);
     let game_dll_path = PathBuf::from(game_dll_abs_path.clone());
 
     // Start file watch for hotloading
     let (reload_pending_send, reload_pending_recv) = sync_channel(1);
-    let fw_thread = {
+    {
         let dll_watcher = Box::new(Game_Dll_File_Watcher::new(
             game_dll_path,
             reload_pending_send,
@@ -60,8 +63,8 @@ fn main() -> std::io::Result<()> {
             std::path::PathBuf::from(GAME_DLL_FOLDER),
             dll_watcher_cfg,
             vec![dll_watcher],
-        )?
-    };
+        )?;
+    }
 
     // Convert rust args to C args, since our game API expects that.
     get_argc_argv!(argc, argv);
@@ -95,8 +98,6 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
-
-    file_watcher::stop_file_watch(fw_thread);
 
     unsafe {
         (game_api.shutdown)(game_state, game_resources);
