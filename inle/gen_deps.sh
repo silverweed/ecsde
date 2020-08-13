bcf7bffb0ae7f1ed074916264219eab3032f1ccb
silverweed <silverweed1991@gmail.com>
2020-08-13 10:51:49 +0200

    move all crates in the inle directory

--- inle/gen_deps.sh
+++ inle/gen_deps.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo 'digraph {' > deps.dot
 for i in $(ls inle_*/Cargo.toml); do
 	for line in $(grep '../inle' $i | cut -f1 -d=); do
-		echo "$(dirname $i) -> $line;"
+		echo -e "\t$(dirname $i) -> $line;"
 	done
-done > deps.dot
+done >> deps.dot
+echo '}' >> deps.dot
