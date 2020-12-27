799b235dd33a49fd4f45dc4c66d035023fa4414a
silverweed <silverweed1991@gmail.com>
2020-12-27 14:51:29 +0100

    reorganize Cargo.toml deps a bit

--- inle/inle_common/src/prelude.rs
+++ inle/inle_common/src/prelude.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 mod misc;
 
-#[cfg(feature = "gfx-sfml")]
+#[cfg(feature = "common-sfml")]
 #[macro_use]
 mod sfml;
 
 pub use misc::*;
 
-#[cfg(feature = "gfx-sfml")]
+#[cfg(feature = "common-sfml")]
 pub use self::sfml::*;
